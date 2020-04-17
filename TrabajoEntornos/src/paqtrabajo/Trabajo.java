c8add4e3e937d944ca7aaa2d5904d62a2d759b21
Galder <Galder@DESKTOP-EU1JRUL>
2020-04-17 21:03:32 +0200

    Tag

--- TrabajoEntornos/src/paqtrabajo/Trabajo.java
+++ TrabajoEntornos/src/paqtrabajo/Trabajo.java
@@ -10,6 +10,7 @@ public class Trabajo {
 		System.out.println("Hola soy Marta");
 		System.out.println("Hola soy Juan");
 		System.out.println("Esto es el branch de prueba");
+		System.out.println("Esto no va a aparecer en el tag");
 
 	}
 
